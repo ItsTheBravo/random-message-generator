5e4f68b0950c16a3244bcec51d10d3eb08b08cda
JPM <johnpmcgrath1996@gmail.com>
2021-03-25 10:42:56 +0000

    Add random phrase output

--- random.js
+++ random.js
@@ -7,11 +7,28 @@ nouns: ["Sheep", "Fish", "Apparatus", "Rice", "Aircraft"],
 numbers: ["2", "3", "4", "5", "6", "7"],
 body_parts: ["Arm", "Leg", "Head", "Foot", "Hand"],
 clothes: ["T-shirt", "Jacket", "Pants", "Shoes", "Hat"],
+};
+
+
+let adj = variables.adjectives[Math.floor(Math.random() * variables.adjectives.length)];
+let names = variables.names[Math.floor(Math.random() * variables.names.length)];
+let nouns = variables.nouns[Math.floor(Math.random() * variables.nouns.length)];
+let numbers = variables.numbers[Math.floor(Math.random() * variables.numbers.length)];
+let body_parts = variables.body_parts[Math.floor(Math.random() * variables.body_parts.length)];
+let clothes = variables.clothes[Math.floor(Math.random() * variables.clothes.length)];
+
+let phrases = {
+    phr: [`Signs point to a very ${adj} yes.`,
+     `The skies are ${adj}. The future is uncertain.`,
+    `I see ${numbers} big ${nouns} in your future.`,
+    `What does your ${body_parts} tell you?`,
+    `Signs point to a very ${adj} no.`,
+    `Picture a/an ${adj} ${nouns}. That is your answer`,
+    `You will find the answer in your ${clothes}.`,
+    `Don't believe anything ${names} says.`,
+    `Signs point to a very ${adj} Yes.`]
 }
 
 
-let phrase1 = variables.adjectives[Math.floor(Math.random() * variables.adjectives.length)];
-let phrase2 = variables.names[Math.floor(Math.random() * variables.names.length)];
-let phrase3 = variables.nouns[Math.floor(Math.random() * variables.nouns.length)];
-
-console.log(`Your secret message is ${phrase1} ${phrase2} ${phrase3}`);
+let result = phrases.phr[Math.floor(Math.random() * variables.clothes.length)];
+console.log(`Your answer is: ${result}` );
