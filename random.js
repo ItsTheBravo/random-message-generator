5e5249a012f7b9575c9527d5e565ea49f91f55af
JPM <johnpmcgrath1996@gmail.com>
2021-03-25 10:26:20 +0000

    Add variables

--- random.js
+++ random.js
@@ -1,14 +1,17 @@
 console.log("Welcome to random message generator!");
 
 let variables = { 
-option1: ["spaghet", "molk", "goop"],
-option2: ["TV", "can", "ps5"],
-option3: ["blanket", "cola", "basket"]
+adjectives: ["Sick", "Naked", "Deep", "Fluffy", "Greedy"],
+names: ["John", "Adam", "Kelly", "Barry", "Grace"],
+nouns: ["Sheep", "Fish", "Apparatus", "Rice", "Aircraft"],
+numbers: ["2", "3", "4", "5", "6", "7"],
+body_parts: ["Arm", "Leg", "Head", "Foot", "Hand"],
+clothes: ["T-shirt", "Jacket", "Pants", "Shoes", "Hat"],
 }
 
 
-let phrase1 = variables.option1[Math.floor(Math.random() * variables.option1.length)];
-let phrase2 = variables.option2[Math.floor(Math.random() * variables.option2.length)];
-let phrase3 = variables.option3[Math.floor(Math.random() * variables.option3.length)];
+let phrase1 = variables.adjectives[Math.floor(Math.random() * variables.adjectives.length)];
+let phrase2 = variables.names[Math.floor(Math.random() * variables.names.length)];
+let phrase3 = variables.nouns[Math.floor(Math.random() * variables.nouns.length)];
 
 console.log(`Your secret message is ${phrase1} ${phrase2} ${phrase3}`);
