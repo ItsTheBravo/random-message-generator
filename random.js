fa4ce56b19c3b982daa01f73540481b9726cca36
JPM <johnpmcgrath1996@gmail.com>
2021-03-25 13:02:09 +0000

    add random function

--- random.js
+++ random.js
@@ -1,4 +1,8 @@
 console.log("Welcome to random message generator!");
+let randomNum = (num) => {
+    let words = num[Math.floor(Math.random() * num.length)];
+    return words;
+};
 
 let variables = { 
 adjectives: ["Sick", "Naked", "Deep", "Fluffy", "Greedy"],
@@ -11,28 +15,28 @@ colours: ["Red", "Blue", "Green", "Yellow", "Purple"],
 };
 
 
-let adj = variables.adjectives[Math.floor(Math.random() * variables.adjectives.length)];
+/*let adj = variables.adjectives[Math.floor(Math.random() * variables.adjectives.length)];
 let names = variables.names[Math.floor(Math.random() * variables.names.length)];
 let nouns = variables.nouns[Math.floor(Math.random() * variables.nouns.length)];
 let numbers = variables.numbers[Math.floor(Math.random() * variables.numbers.length)];
 let body_parts = variables.body_parts[Math.floor(Math.random() * variables.body_parts.length)];
 let clothes = variables.clothes[Math.floor(Math.random() * variables.clothes.length)];
-let colours = variables.colours[Math.floor(Math.random() * variables.colours.length)];
+let colours = variables.colours[Math.floor(Math.random() * variables.colours.length)]; */
 
 let phrases = {
-    phr: [`Signs point to a very ${adj} yes.`,
-     `The skies are ${adj}. The future is uncertain.`,
-    `I see ${numbers} big ${nouns} in your future.`,
-    `What does your ${body_parts} tell you?`,
-    `Signs point to a very ${adj} no.`,
-    `Picture a/an ${adj} ${nouns}. That is your answer`,
-    `You will find the answer in your ${clothes}.`,
-    `Don't believe anything ${names} says.`,
-    `Signs point to a very ${adj} Yes.`]
+    phr: [`Signs point to a very ${randomNum(variables.adjectives)} yes.`,
+     `The skies are ${randomNum(variables.adjectives)}. The future is uncertain.`,
+    `I see ${randomNum(variables.numbers)} big ${randomNum(variables.nouns)} in your future.`,
+    `What does your ${randomNum(variables.body_parts)} tell you?`,
+    `Signs point to a very ${randomNum(variables.adjectives)} no.`,
+    `Picture a/an ${randomNum(variables.adjectives)} ${randomNum(variables.nouns)}. That is your answer`,
+    `You will find the answer in your ${randomNum(variables.clothes)}.`,
+    `Dont believe anything ${randomNum(variables.names)} says.`,
+    `Signs point to a very ${randomNum(variables.adjectives)} Yes.`]
 }
 
 
-let result = phrases.phr[Math.floor(Math.random() * variables.clothes.length)];
+let result = phrases.phr[Math.floor(Math.random() * phrases.phr.length)];
 console.log(`Your answer is: ${result}
- Your lucky number is: ${numbers}
- Your special colour is: ${colours}` );
+ Your lucky number is: ${randomNum(variables.numbers)}
+ Your special colour is: ${randomNum(variables.colours)}` );
